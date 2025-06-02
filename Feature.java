1a9be1e06ac6f0054b7435adf95892500969fd22
samissua <samissua@espol.edu.ec>
2025-06-02 14:55:37 -0500

    Initial Commit

---
+++ Feature.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.samissua.gym_membership;
+
+/**
+ *
+ * @author CltControl
+ */
+public class Feature {
+    private String name;
+    private Float cost;
+    
+    public Feature(String name, Float cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getCost() {
+        return cost;
+    }
+
+    public void setCost(Float cost) {
+        this.cost = cost;
+    }
+}
